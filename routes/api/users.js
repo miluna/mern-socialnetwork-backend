f0f1ad4940d9a5f3381fada4da64a8b2cc747a15
Miguel Angel Luna <miluna1994@gmail.com>
2018-06-17 20:35:00 +0200

    ..

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+// Load User model
+const User = require("../../models/User");
 
 // @route       GET api/users/test
 // @desc        Tests user route
@@ -9,5 +14,43 @@ router.get("/test", (req, res) =>
         msg: "Users Works"
     })
 );
+
+// @route       POST api/users/register
+// @desc        Register user
+// @access      Public
+router.post("/register", (req, res) => {
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            return res.status(400).json({
+                error: "Email already exists"
+            });
+        } else {
+            // get avatar from gravatar using email
+            const avatar = gravatar.url(req.body.email, {
+                s: 200, // Size
+                r: "pg", // Rating
+                d: "mm" // Default image
+            });
+
+            // create new user instance
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                avatar,
+                password: req.body.password
+            });
+            // hash the password and save the user
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+});
 
 module.exports = router;
